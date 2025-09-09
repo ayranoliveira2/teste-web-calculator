55144398f87f460aea70bb971f656261f09c812c
Ayran <ayran@inspell.com.br>
2025-09-09 08:01:14 -0300

    feat(types): adiciona tipos para Periodo

---
+++ src/types/periodo.ts
@@ -0,0 +1,28 @@
+import z from "zod";
+
+export const createPeriodoSchema = z.object({
+  dataInicial: z
+    .string("dataInicial deve ser uma data válida")
+    .min(1, { message: "dataInicial é obrigatório" }),
+  dataFinal: z
+    .string("dataFinal deve ser uma data válida")
+    .min(1, { message: "dataFinal é obrigatório" }),
+});
+
+export type CreatePeriodoInput = z.infer<typeof createPeriodoSchema>;
+
+export interface Periodo {
+  id: string;
+  dataInicial: Date;
+  dataFinal: Date;
+  duracaoSegundos: number;
+}
+
+export interface PeriodosResponse {
+  periodos: Periodo[];
+  total: {
+    horas: string;
+    minutos: string;
+    segundos: string;
+  };
+}
